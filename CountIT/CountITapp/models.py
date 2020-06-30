85ca3010ca1eb792e5e457cc7434a3bd97067916
vladimirantigua <vladimirantigua@gmail.com>
2020-06-29 18:48:41 -0700

    completed my models Capstone

--- CountIT/CountITapp/models.py
+++ CountIT/CountITapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Inventory(models.Model):
+    equipment_name = models.CharField(max_length=200)
+    equipment_model = models.CharField(max_length=200)
+    purchase_date = models.DateTimeField()
+    expiration_date = models.DateTimeField()
+    quantity = models.IntegerField(default='0')
+
+    def __str__(self):
+        return self.equipment_name + ' - ' + self.equipment_model + ' - ' + self.quantity
+
+# check with the Matthew or TAs to see if my models are correct
+# see if I need to create a users models login or if I can use the build in Django user login check if need to create a user login pw if using the built in user that was created
