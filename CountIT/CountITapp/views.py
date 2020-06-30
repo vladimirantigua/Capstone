b2f709289116b72bc32b54ba2937db9ed5fa5c45
vladimirantigua <vladimirantigua@gmail.com>
2020-06-30 11:28:21 -0700

    added  Render Data Template & background images

--- CountIT/CountITapp/views.py
+++ CountIT/CountITapp/views.py
@@ -6,8 +6,12 @@ from .models import Inventory
 # Create your views here.
 
 
+# retrieve data from the database and pass it to the template
 def index(request):
+    # Order by quantity the items with less items at the top
+    inventory_items = Inventory.objects.order_by('quantity')
+    # inventory_items = CountIT.objects.all().order_by('equipment_model')
     context = {
-        'message': 'Count IT :) !'
+        'inventory_items': inventory_items
     }
     return render(request, 'CountITapp/index.html', context)
