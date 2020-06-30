bc2dcb4f743099709da26666ef40af1f20dbc6fe
vladimirantigua <vladimirantigua@gmail.com>
2020-06-29 21:50:05 -0700

    updated Models returned concateneded items

--- CountIT/CountITapp/models.py
+++ CountIT/CountITapp/models.py
@@ -21,7 +21,7 @@ class Inventory(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
-        return self.equipment_name + ' - ' + self.equipment_model + ' - ' + self.quantity
+        return self.equipment_name + ' - ' + self.equipment_model + ' - ' + self.asset_tag + ' - ' + self.service_tag
 
 # check with the Matthew or TAs to see if my models are correct
 # see if I need to create a users models login or if I can use the build in Django user login check if need to create a user login pw if using the built in user that was created
