d94267e6354334086b265a869ef6e1422346d1a1
vladimirantigua <vladimirantigua@gmail.com>
2020-07-06 07:42:14 -0700

    all path working but path edit submit doesn't work

--- CountIT/CountITapp/urls.py
+++ CountIT/CountITapp/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     # path('', views.home, name='home'),
     path('detail/<int:id>/', views.detail, name='detail'),
     path('add_equipment/', views.add_equipment, name='add_equipment'),
-    path('edit_equipment/<int:id>/', views.add_equipment, name='edit_equipment'),
-    path('edit_equipment/submit/', views.edit_submit, name='edit_item_submit'),
-    path('delete/<int:id>/', views.add_equipment, name='delete'),
+    path('edit_equipment/<int:id>/', views.edit_equipment, name='edit_equipment'),
+    # path('edit_equipment/submit/', views.edit_submit, name='edit_item_submit'),
+    path('delete/<int:id>/', views.delete, name='delete'),
     path('logout/', views.logout_user, name='logout_user'),
     path('search/', views.search, name='search'),
 
